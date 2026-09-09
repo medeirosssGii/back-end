e44cb6cf0992474a42bc0ddc7416f636f2fbc7bd
medeirosssGii <giovanna.f@edu.senai.br>
2026-09-09 17:20:31 -0300

    Add files via upload

---
+++ list10/atv12.php
@@ -0,0 +1,9 @@
+<?php
+
+$nome = "Nicolas Dante";
+if (isset($nome)) {
+    echo "Nome: ", $nome;
+} else {
+    echo "Por favor, preencha o seu nome.";
+}
+?>
---
+++ list10/atv4.php
@@ -0,0 +1,11 @@
+<?php
+$nome_cliente = "Luccas Kenzo Cremer Abe";
+
+echo "Cliente: ". ltrim($nome_cliente);
+
+echo "<br>";
+
+$nome_cliente2 = "Giovanna Medeiros Medrado Ao Quadrado";
+
+echo "Cliente: ". rtrim($nome_cliente2);
+?>
